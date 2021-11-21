90753ab9298c2de03cba89a483e91f80e3516aab
Venkatesh Anandas <41284136+venkatesh5u3@users.noreply.github.com>
2021-11-21 21:39:38 +0530

    Add files via upload
    Assignment 3

---
+++ summation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[48]:
+
+
+def summation(mylist):
+    s=0
+    for i in mylist:
+        s+=i
+    return s
+my_list=[8, 2, 3, 0, 7]
+print(summation(my_list))
+
+
+# In[ ]:
+
+
+
+
